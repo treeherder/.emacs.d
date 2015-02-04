364dd73ae61f9553d4ced52d526e513b123924ea
treeherder <b.d.reddybest@gmail.com>
2015-02-04 00:26:29 -0800

    took out shit modes

--- init.el
+++ init.el
@@ -93,7 +93,7 @@
 ;;
 ;; (require 'yaml-mode)
 ;; (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-;; 
+;;
 ;; Adding this code will make Emacs enter yaml mode whenever you open
 ;; a .yml file
 (add-to-list 'load-path "~/.emacs.d/vendor")
@@ -142,15 +142,13 @@
 ;; Disable annoying UI
  (setq inhibit-splash-screen t)
  (tool-bar-mode -1)
- 
+
  ;; UI preferences
  (add-to-list 'default-frame-alist '(font . "Inconsolata-14"))
  (set-face-attribute 'default t :font "Inconsolata-14")
- (load-theme 'tangotango t)
- 
  ;; Backups
  (setq backup-directory-alist '(("." . "~/.emacs.d/saves")))
- 
+
  ;; Indentation / Whitespace
  (setq-default indent-tabs-mode nil)
  (setq default-tab-width 4)
@@ -159,17 +157,3 @@
 
 ;; Insert matching pairs automatically
  (electric-pair-mode t)
- 
- ;; Git
- (require 'magit)
- (require 'magit-gh-pulls)
- (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
- 
- ;; JavaScript
- (require 'js2-mode)
- (autoload 'js2-mode "js2-mode" nil t)
- (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
- 
- ;; SASS
- (add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
- (add-hook 'scss-mode-hook 'rainbow-mode)
